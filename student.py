eefc1bfd84044dcab946323276bf3b7a7d1349b7
nareshnarayanans2023-eng <nareshnarayanan.s2023@vitstudent.ac.in>
2026-01-08 21:35:29 +0530

    Wrong code added

--- student.py
+++ student.py
@@ -2,3 +2,4 @@ def register_student(name, roll_no):
     print("Student Registered:", name, roll_no)
 def course(coursen,coursecode):
     print("course name:",coursen,coursecode)
+    print("hello")
